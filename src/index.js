0a06445973b9bf4d0e89872c0591001a50417db1
proartmateur <ennimandroid@gmail.com>
2019-09-28 12:01:28 -0500

    Paso 1 y 2

--- src/index.js
+++ src/index.js
@@ -1,36 +1,112 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+//const API = 'https://rickandmortyapi.com/api/character/';
+const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+
+
+
+function renderItem(output){
+  let newItem = document.createElement('section');
+  newItem.classList.add('Items');
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
+}
+
+function existeNextFetch(){
+  let hay_next_fetch = Object.keys(localStorage).find( key => {
+    return key === "next_fetch"
+  })
+
+  if (hay_next_fetch === "next_fetch") {
+      return true
+  } else {
+    return false
+  }
+}
 
 const getData = api => {
   fetch(api)
-    .then(response => response.json())
     .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      console.log("Respuesta:", response)
+      if(response.status === 200){
+        return response.json()
+      }else{
+        return { 
+          "load_error": "No se pudo obtener la lista de personajes, intenta más tarde"
+         }
+      }      
+    })
+    .then(response => {
+      let hay_load_error = Object.keys(response).find( key => {
+        return key === "load_error"
+      })
+      console.log(hay_load_error)
+      if(hay_load_error === undefined){
+        
+        let next_page = response.info.next
+        let total_pages = response.info.pages
+
+
+       
+        
+        if ( existeNextFetch() ) {
+          console.log("Existe Next Fetch")
+          let current_page = localStorage.getItem("next_fetch")
+          if(current_page === next_page){
+            console.log("Son iguales las páginas")
+          }else{
+            localStorage.setItem("next_fetch", next_page)
+          }
+        }else{
+          localStorage.setItem("next_fetch", next_page)
+        }
+        
+
+        const characters = response.results;
+        let output = characters.map(character => {
+          return `
+        <article class="Card">
+          <img src="${character.image}" />
+          <h2>${character.name}<span>${character.species}</span></h2>
+        </article>
+      `
+        }).join('');
+        renderItem(output)
+
+      }else{
+        let output = `
+          <div class="load_error"> ${response.load_error} </div>
+        `
+        renderItem(output)
+      }
     })
     .catch(error => console.log(error));
 }
 
 const loadData = () => {
-  getData(API);
+
+  if ( existeNextFetch() ) {
+    console.log("Existe Next Fetch, Loading new data....")
+    let next_fetch = localStorage.getItem("next_fetch")
+    if(next_fetch === ""){
+      console.log('No hay más páginas')
+    }else{
+      getData(next_fetch);
+    }
+    
+  }else{
+    getData(API);
+  }
+
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   if (entries[0].isIntersecting) {
+    console.log("Es visible")
     loadData();
-  }
+  } else {
+    console.log("No se ves")
+  } 
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
